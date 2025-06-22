547a29c0cced0c98177569a2adcde0cee94842d8
Dilkash Raza <dilkashsns@gmail.com>
2025-06-22 14:33:12 +0530

    setup shadcn-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
-    <>
-      <h1 className="">Hello jee kais ho mitron</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   );
 }
 
